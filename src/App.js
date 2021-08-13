5a83c09dc0ef8906a1dbfe97ac798e6100d1e8e0
Your Name <tkorhonen13@gmail.com>
2021-08-13 17:02:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react';
+import {HashRouter as Router, Switch, Route} from "react-router-dom"
+import ScrollToTop from './components/ScrollToTop';
+import Navbar from "./components/Navbar"
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router >
+      <ScrollToTop />
+      <Navbar toggle={toggle}/>
+      
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { MenuIcon } from "@heroicons/react/solid"
+
+const Navbar = ({toggle}) => {
+    return (
+        <nav className="sticky top-0 z-50 grid
+        grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
+            {/* left */}
+            <div className="flex items-center">
+                <span className="text-2xl">VBank</span>
+            </div>
+            {/* middle */}
+            <div className="md:flex items-center justify-center hidden ">
+                <ul className="centerFlex gap-4 sm:flex-wrap text-lg items-center">
+                    <li>About</li>
+                    <li>Discover</li>
+                    <li>Services</li>
+                    <li>Sign up</li>
+                </ul>
+            </div>
+            {/* right */}
+            <div className="flex items-center md:justify-end justify-center">
+                <button className="btn shadow-sm">Sign In</button>
+            </div>
+            <div className="md:hidden px-4 flex items-center justify-end">
+                <MenuIcon className="h-16"/>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
